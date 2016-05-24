b1e9681a14c9244bce1d599a87d06b5f305f5f22
Luca Hofmann <HofmannLuca@gmail.com>
2016-05-24 17:56:10 +0200

    Cleanup code.

--- Assets/Editor/Bon/EditorZoomArea.cs
+++ Assets/Editor/Bon/EditorZoomArea.cs
@@ -11,11 +11,9 @@ namespace Assets.Editor.Bon
 	public class EditorZoomArea
 	{
 
-
-		private static Matrix4x4 _prevGuiMatrix;
-
-		private static Vector3 _tmpScaleVector = new Vector3();
-		private static Rect _tmpRect = new Rect();
+		private static Matrix4x4 prevGuiMatrix;
+		private static Vector3 tmpScaleVector = new Vector3();
+		private static Rect tmpRect = new Rect();
 
 		public static Rect Begin(float zoomScale, Rect screenCoordsArea)
 		{
@@ -23,20 +21,20 @@ namespace Assets.Editor.Bon
 			Rect clippedArea = screenCoordsArea.ScaleSizeBy(1.0f / zoomScale, screenCoordsArea.TopLeft());
 			clippedArea.y += BonWindow.TopOffset;
 			GUI.BeginGroup(clippedArea);
-			_prevGuiMatrix = GUI.matrix;
+			prevGuiMatrix = GUI.matrix;
 			Matrix4x4 translation = Matrix4x4.TRS(clippedArea.TopLeft(), Quaternion.identity, Vector3.one);
-			_tmpScaleVector.Set(zoomScale, zoomScale, 1.0f);
-			Matrix4x4 scale = Matrix4x4.Scale(_tmpScaleVector);
+			tmpScaleVector.Set(zoomScale, zoomScale, 1.0f);
+			Matrix4x4 scale = Matrix4x4.Scale(tmpScaleVector);
 			GUI.matrix = translation * scale * translation.inverse * GUI.matrix;
 			return clippedArea;
 		}
 
 		public static void End()
 		{
-			GUI.matrix = _prevGuiMatrix;
+			GUI.matrix = prevGuiMatrix;
 			GUI.EndGroup();
-			_tmpRect.Set(0, BonWindow.TopOffset, Screen.width, Screen.height);
-			GUI.BeginGroup(_tmpRect);
+			tmpRect.Set(0, BonWindow.TopOffset, Screen.width, Screen.height);
+			GUI.BeginGroup(tmpRect);
 		}
 	}
 }
--- Assets/Editor/Bon/RectExtensions.cs
+++ Assets/Editor/Bon/RectExtensions.cs
@@ -9,17 +9,19 @@ namespace Assets.Editor.Bon
 	public static class RectExtensions
 	{
 
-		private static Vector2 _tmpTopLeft = new Vector2();
+		private static Vector2 tmpTopLeft = new Vector2();
 
 		public static Vector2 TopLeft(this Rect rect)
 		{
-			_tmpTopLeft.Set(rect.xMin, rect.yMin);
-			return _tmpTopLeft;
+			tmpTopLeft.Set(rect.xMin, rect.yMin);
+			return tmpTopLeft;
 		}
+
 		public static Rect ScaleSizeBy(this Rect rect, float scale)
 		{
 			return rect.ScaleSizeBy(scale, rect.center);
 		}
+
 		public static Rect ScaleSizeBy(this Rect rect, float scale, Vector2 pivotPoint)
 		{
 			Rect result = rect;
@@ -33,10 +35,12 @@ namespace Assets.Editor.Bon
 			result.y += pivotPoint.y;
 			return result;
 		}
+
 		public static Rect ScaleSizeBy(this Rect rect, Vector2 scale)
 		{
 			return rect.ScaleSizeBy(scale, rect.center);
 		}
+
 		public static Rect ScaleSizeBy(this Rect rect, Vector2 scale, Vector2 pivotPoint)
 		{
 			Rect result = rect;
