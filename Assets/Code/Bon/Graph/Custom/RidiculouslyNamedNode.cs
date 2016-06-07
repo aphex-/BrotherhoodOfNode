7f6a0b99b8cedd8988db3517dba87a1629829931
Luca Hofmann <HofmannLuca@gmail.com>
2016-06-07 19:17:49 +0200

    using tabs as whitespaces

--- Assets/Code/Bon/Graph/Custom/RidiculouslyNamedNode.cs
+++ Assets/Code/Bon/Graph/Custom/RidiculouslyNamedNode.cs
@@ -3,23 +3,23 @@ using UnityEngine;
 
 namespace Assets.Code.Bon.Graph.Custom
 {
-    [Serializable]
-    [GraphContextMenuItem("Standard", "CoolNamedNode")]
-    public class RidiculouslyNamedNode : Node
-    {
-        public RidiculouslyNamedNode(int id) : base(id)
-        {
-            Sockets.Add(new Socket(this, Color.red, true));
-            Height = 65;
-        }
+	[Serializable]
+	[GraphContextMenuItem("Standard", "CoolNamedNode")]
+	public class RidiculouslyNamedNode : Node
+	{
+		public RidiculouslyNamedNode(int id) : base(id)
+		{
+			Sockets.Add(new Socket(this, Color.red, true));
+			Height = 65;
+		}
 
-        public override void OnGUI()
-        {
-        }
+		public override void OnGUI()
+		{
+		}
 
-        public override void ApplySerializationData(SerializableNode sNode)
-        {
-            sNode.data = JsonUtility.ToJson(this);
-        }
-    }
+		public override void ApplySerializationData(SerializableNode sNode)
+		{
+			sNode.data = JsonUtility.ToJson(this);
+		}
+	}
 }
